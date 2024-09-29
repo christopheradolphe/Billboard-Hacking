90ae203d2c19e3d95c182ca2b6b3ea11046e089d
christopheradolphe <ckwadolphe@gmail.com>
2024-09-29 18:55:19 -0400

    Create the perspective homography matrix map and DLT matrix

--- templates/dlt_homography.py
+++ templates/dlt_homography.py
@@ -19,8 +19,33 @@ def dlt_homography(I1pts, I2pts):
     H  - 3x3 np.array of perspective homography (matrix map) between image coordinates.
     A  - 8x9 np.array of DLT matrix used to determine homography.
     """
-    #--- FILL ME IN ---
+    u = I1pts[0, :]
+    v = I1pts[1, :]
+    x = I2pts[0, :]
+    y = I2pts[1, :]
+    A = np.array()
 
-    #------------------
+    for point in range(u.shape[1]):
+        two_equations = np.array([
+            [-x[point], -y[point], -1,  0,    0,    0,   u[point]*x[point],  u[point]*y[point],  u[point]],
+            [ 0,     0,    0, -x[point], -y[point], -1,  v[point]*x[point],  v[point]*y[point],  v[point]]
+        ])
+        A = np.append(A, two_equations)
+    
+    A = np.array([
+        # First point Correspondance
+        [-x[0], -y[0], -1,  0,    0,    0,   u[0]*x[0],  u[0]*y[0],  u[0]],
+        [ 0,     0,    0, -x[0], -y[0], -1,   v[0]*x[0],  v[0]*y[0],  v[0]]
+        # Second Point Correspondance
+        [],
+        [],
+        # Third Point Correspondance
+        [],
+        [],
+        # Fourth Point Correspondance
+        [],
+        []
+
+    ])
 
     return H, A
